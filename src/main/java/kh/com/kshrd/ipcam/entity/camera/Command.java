1d2ef0789c189d248e4079d606276c3ab1a773fc
rina <chhenrina@gmail.com>
2016-12-15 16:23:04 +0700

    update

--- src/main/java/kh/com/kshrd/ipcam/entity/camera/Command.java
+++ src/main/java/kh/com/kshrd/ipcam/entity/camera/Command.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import kh.com.kshrd.ipcam.entity.base.BaseEntity;
 
 public class Command extends BaseEntity{
-	
-	private Map<String, Object> body;
+
+	private int command_id ;
+	private String command;
+	private String  body;
 	
 	public Command() {
 		
