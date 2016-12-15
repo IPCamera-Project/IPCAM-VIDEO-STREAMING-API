039bdfc8e730b4a1607747e59f210d633611e3f7
rina <chhenrina@gmail.com>
2016-12-15 14:58:11 +0700

    configured datasource and security

--- src/main/java/ipcam/knongdai/configuration/MybatisConfiguration.java
+++ src/main/java/ipcam/knongdai/configuration/MybatisConfiguration.java
@@ -1,8 +1,44 @@
 package ipcam.knongdai.configuration;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
+@MapperScan("ipcam.knongdai.repository")
+@EnableSwagger
 public class MybatisConfiguration {
+    @Autowired
+    private DataSource datasource;
+
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://120.136.24.174:5432/IPCAM_MANAGEMENT_DB");
+        dataSource.setUsername("IPCAM_USER");
+        dataSource.setPassword("IPCAM_USER");
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlsessionfactorybean(){
+        SqlSessionFactoryBean sqlsessionfactoryBean = new SqlSessionFactoryBean();
+        sqlsessionfactoryBean.setDataSource(datasource);
+        return sqlsessionfactoryBean;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(){
+        return new DataSourceTransactionManager(datasource);
+    }
 
 }
--- src/main/java/ipcam/knongdai/configuration/WebMvcConfiguration.java
+++ src/main/java/ipcam/knongdai/configuration/WebMvcConfiguration.java
@@ -1,8 +1,21 @@
 package ipcam.knongdai.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebMvcConfiguration {
-
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger").setViewName("/swagger");
+        registry.addViewController("/login").setViewName("/login");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET","POST","DELETE","PUT","OPTIONS","PATCH")
+                .allowedOrigins("*");
+    }
 }
