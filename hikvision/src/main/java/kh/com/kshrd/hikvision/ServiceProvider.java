a6f1d718c6153d32f834f86d59752cea5688a6d5
sophatvathana <sovathana.phat@gmail.com>
2016-12-21 08:07:47 +0700

    Fixed Pom

--- hikvision/src/main/java/kh/com/kshrd/hikvision/ServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package kh.com.kshrd.hikvision;
-
-import kh.com.kshrd.core.*;
-/**
- * Created by sophatvathana on 19/12/16.
- */
-public class ServiceProvider implements Plugin{
-
-}
--- src/main/java/kh/com/kshrd/ipcam/IpcamVideoStreamingApiApplication.java
+++ src/main/java/kh/com/kshrd/ipcam/IpcamVideoStreamingApiApplication.java
@@ -1,15 +1,18 @@
 package kh.com.kshrd.ipcam;
 
+import kh.com.kshrd.ipcam.controller.camera.IPCamControlController;
 import kh.com.kshrd.ipcam.entity.camera.Command;
 import kh.com.kshrd.ipcam.service.impl.CommandServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@Controller
+@ComponentScan(basePackageClasses = IPCamControlController.class)
+//@ComponentScan
 public class IpcamVideoStreamingApiApplication {
 
 	public static void main(String[] args) {
